24ec2c9d0fe5b6db6ca6da62e747722a17bee80e
isaias <isaias.villa123456@gmail.com>
2026-09-17 19:32:22 -0400

    finished adding readme doument and formated  index.js

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 let first = prompt("whats your first name?");
 let last = prompt("whats your last name?");
 
-function formatName(firstName, lastName){
-
-    if (firstName  == "" || lastName == ""){
-        return "invalid name input"
-    }
-
-    else {
+function formatName(firstName, lastName) {
+  if (firstName == "" || lastName == "") {
+    return "invalid name input";
+  } else {
     let fn = firstName.charAt(0).toUpperCase() + firstName.slice(1);
     let ln = lastName.charAt(0).toUpperCase() + lastName.slice(1);
     let fullName = fn + " " + ln;
 
-    return (fullName);
-    }
-
+    return fullName;
+  }
 }
 
-console.log(formatName(first,last));
+console.log(formatName(first, last));
 
 let price = parseFloat(prompt("type the price"));
 let quantity = parseFloat(prompt("type the quantity"));
@@ -25,22 +21,25 @@ let taxRate = parseFloat(prompt("type the tax rate"));
 let discount = 0;
 
 function calculateTotalCost(price, quantity, taxRate) {
-  let hasDiscount = parseFloat(prompt("do you have a discount type 1 for yes or 0 for no"));
+  let hasDiscount = parseFloat(
+    prompt("do you have a discount type 1 for yes or 0 for no"),
+  );
 
   if (isNaN(hasDiscount)) {
     return "invalid input";
   } 
+  //has discount
   else if (hasDiscount === 1) {
     discount = parseFloat(prompt("type your discount"));
     if (isNaN(price) || isNaN(quantity) || isNaN(taxRate) || isNaN(discount)) {
       return "invalid input";
-    } 
-    else {
-      let totalCost = ((price * quantity) - discount) * (1 + taxRate);
+    } else {
+      let totalCost = (price * quantity - discount) * (1 + taxRate);
       return totalCost;
     }
   } 
-  else if(hasDiscount === 0){
+  //no discount
+  else if (hasDiscount === 0) {
     if (isNaN(price) || isNaN(quantity) || isNaN(taxRate)) {
       return "invalid input";
     } else {
@@ -55,17 +54,13 @@ console.log(calculateTotalCost(price, quantity, taxRate));
 let age = Number(window.prompt("type your age", ""));
 let isEmployed = window.prompt("Are you employed?").toLowerCase() === "yes";
 
-function checkEligibility(age, isEmployed){
-
-    if(age > 18 && isEmployed === true){
-        return "you are eligible";
-    }
-    else if(age >= 18 && isEmployed === false){
-        return "you are conditionally eligible";
-    }
-    else if(age < 18){
-        return "you are not eligible"
-
-    }
+function checkEligibility(age, isEmployed) {
+  if (age > 18 && isEmployed === true) {
+    return "you are eligible";
+  } else if (age >= 18 && isEmployed === false) {
+    return "you are conditionally eligible";
+  } else if (age < 18) {
+    return "you are not eligible";
+  }
 }
-console.log(checkEligibility(age, isEmployed))
+console.log(checkEligibility(age, isEmployed));
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 How did you approach creating more flexible functions with parameters?
 
-i created variables that
+*i created variables that a user can input information into and that data is then pluged into functions that give specific outputs
 
 What challenges did you face while incorporating conditional logic in your functions?
 
-
+*the isEmployed variable gave me trouble implementing since at first it wasn't working but i eventually fixed th code
 
 How does refactoring improve the readability and maintainability of your code?
+
+*refactoring improves readability and maintainability when you section off what parts you add a discount and when you don't have the discount. it also helps when you use comments on the sections to tell them further apart
